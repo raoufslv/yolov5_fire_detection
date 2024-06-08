755a2230cf499d096cd00dc830e985b67fdc7e30
Raouf <raoufslv09@gmail.com>
2024-06-09 00:31:14 +0100

    added a gpt arch

--- models/common.py
+++ models/common.py
@@ -1223,3 +1223,20 @@ class Involution(nn.Module):
             out = (weight * out).sum(dim=3).view(b, self.c1, h, w)
 
             return out
+
+class SEBlock(nn.Module):
+    def __init__(self, in_channels, reduction=16):
+        super(SEBlock, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(in_channels, in_channels // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_channels // reduction, in_channels, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
